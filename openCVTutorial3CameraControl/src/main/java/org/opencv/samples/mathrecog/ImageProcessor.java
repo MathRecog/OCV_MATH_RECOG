c8bbe5f932684604ad7768ae6361a9bc95198ed8
vgerasim <gerkarasta@gmail.com>
2015-03-02 01:22:39 +0300

    Find contours: done

--- openCVTutorial3CameraControl/src/main/java/org/opencv/samples/mathrecog/ImageProcessor.java
+++ openCVTutorial3CameraControl/src/main/java/org/opencv/samples/mathrecog/ImageProcessor.java
@@ -50,33 +50,57 @@ public class ImageProcessor {
 
         // thersh hold (make the image binary)
         adaptiveThreshold(patient, patient, 255, ADAPTIVE_THRESH_MEAN_C, THRESH_BINARY, 35, 35);
-        erode(patient, patient, getStructuringElement(MORPH_RECT, new Size(2, 4)));
+//        erode(patient, patient, getStructuringElement(MORPH_RECT, new Size(2, 4)));
+
         imwrite("/sdcard/MathRecogData/__ats__" + debug, patient);
 
         // find lines
         Mat zeros = new Mat(patient.rows(), patient.cols(), patient.type(), new Scalar(255, 255, 255));
         subtract(zeros, patient, patient);
-        for(int t = 0; t < 2; t++) {
+        for(int t = 0; t < 3; t++) {
             Mat lines = new Mat();
-            HoughLinesP(patient, lines, 1, Math.PI / 180, 50, patient.height() / 2, 7);
-            Log.i("Lines count:", String.valueOf(lines.cols()));
-            if (lines.cols() > 10) {
+            HoughLinesP(patient, lines, 5, Math.PI / 180, 30, 200, 15);
+            Log.i("Lines found", String.valueOf(lines.cols()));
+            if (lines.cols() > 17) {
                 for (int i = 0; i < lines.cols(); i++) {
                     double[] val = lines.get(0, i);
-                    line(patient, new Point(val[0], val[1]), new Point(val[2], val[3]), new Scalar(0, 0, 0), 3);
-                    Log.i("New Line:", "("+val[0]+","+val[1]+") - ("+val[2]+","+val[3]+")");
+                    // y = kx + b
+                    double k = (val[1] - val[3]) / (val[0] - val[2]);
+                    double b = val[1] - k*val[0];
+                    line(photo, new Point(0, (int)b), new Point(photo.cols(), (int)(k*photo.cols() + b) ), new Scalar(0, 200, 100), 4);
+                    line(patient, new Point(0, (int)b), new Point(photo.cols(), (int)(k*photo.cols() + b) ), new Scalar(0, 0, 0), 4);
                 }
             }
         }
+        imwrite("/sdcard/MathRecogData/__lines__" + debug, photo);
+
         // connect closest black points
-        int w = patient.cols();
-        int h = patient.rows();
-        for (int j = 0; j < h - (h % 9); j += 9 ) {
-            for (int k = 0; k < w - (w % 9); k+=9 ) {
-                Mat tmp = patient.submat(j, j+9, k, k+9);
-                double[] sum = getSum(tmp);
-                if (sum > 255 * 6) {
-                    rectangle(patient, new Point(k, j), new Point(k + 8, j + 8), new Scalar(255, 255, 255), -1);
+        for(int iter = 0; iter < 2; iter++ ) {
+            int w = patient.cols() + iter * 5;
+            int h = patient.rows() + iter * 5;
+            int index = (3+iter);
+            int koef = index*index;
+            for (int j = 0; j < h - (h % koef); j += koef) {
+                for (int k = 0; k < w - (w % koef); k += koef) {
+                    Mat tmp = patient.submat(j, j + koef, k, k + koef);
+                    double[] sum = getSum(tmp, koef);
+                    if (sum[0] > 255 * koef * koef * 0.15) {
+                        for (int x = 1; x < koef; x++) {
+                            if (sum[x] >= 4 * 255) {
+                                int x1 = k + ((x - 1) % 3) * 3 + 1;
+                                int y1 = j + ((int) ((x - 1) / 3)) * 3 + 1;
+                                for (int y = x + 1; y < 10; y++) {
+                                    if (sum[y] >= 4 * 255) {
+                                        int x2 = k + ((y - 1) % 3) * 3 + 1;
+                                        int y2 = j + ((int) ((y - 1) / 3)) * 3 + 1;
+                                        line(patient, new Point(x1, y1), new Point(x2, y2), new Scalar(255, 255, 255), 2);
+                                    }
+                                }
+                            }
+                        }
+                    } else {
+                        rectangle(patient, new Point(k, j), new Point(k + koef-1, j + koef-1), new Scalar(0, 0, 0), -1);
+                    }
                 }
             }
         }
@@ -91,11 +115,15 @@ public class ImageProcessor {
 
         // get average contour length
         double aver = 0.0;
+        double min = Double.MAX_VALUE;
+        double max = Double.MIN_VALUE;
         for (int i = 0; i < conts.size(); i++) {
+            double value = arcLength(new MatOfPoint2f(conts.get(i).toArray()), false);
             aver += arcLength(new MatOfPoint2f(conts.get(i).toArray()), false);
+            if( value > max ) { max = value; }
+            if( value < min ) { min = value; }
         }
         aver = aver / conts.size();
-
         // get submats by contours bounding rectangles
         List<Mat> separatedConts = new ArrayList<Mat>();
         List<MatOfPoint> contsLong = new ArrayList<MatOfPoint>();
@@ -110,12 +138,6 @@ public class ImageProcessor {
         }
 
         imwrite("/sdcard/MathRecogData/__rect__" + debug, patient);
-        Size s1 = new Size();
-        Point p10 = new Point();
-        separatedConts.get(20).locateROI(s1, p10);
-        Size s2 = new Size();
-        Point p20 = new Point();
-        separatedConts.get(21).locateROI(s2, p20);
         //       Mat test = symbolsCont.submat();
         // 2nd PART: try to recognize symbols
 
@@ -138,15 +160,18 @@ public class ImageProcessor {
     }
 
     private double[] getSum(Mat m) {
-        double[] out = new double[10]; // 1 - 1,1, 2 - 1,2, 3 - 1,3, 4 - 2,1.....
-        double sum = 0;
+        double[] out = new double[10]; // 0 - 1,1, 1 - 1,2, 2 - 1,3, 3 - 2,1.....
+        for(int k = 0; k < 10; k++) { out[k] = 0; }
+
         for(int i = 0; i < m.cols(); i++){
             for(int t = 0; t < m.rows(); t++){
                 double[] data = m.get(t, i);
-                sum += data[0]; //for an image in grayscale
+                out[0] += data[0]; //for an image in grayscale
+                int index = (int)(i / 3) + 3 * (int)(t / 3);
+                out[index] += data[0];
             }
         }
-        return sum;
+        return out;
     }
 
     private int getMaxSum(Mat m){
